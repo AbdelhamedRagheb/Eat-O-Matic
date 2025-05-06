04054c7afa7637b24e2659d00eb3fcf30a26db4d
Esraa Medhat <esraamedhat1315603@gmail.com>
2025-05-06 21:03:09 +0300

    Home & About Page

--- src/components/ActionButtons/ActionButtons.jsx
+++ src/components/ActionButtons/ActionButtons.jsx
@@ -8,7 +8,7 @@ export default function ActionButtons({ onAsk }) {
       
       <Link
         to="/menu"
-        className="flex items-center space-x-2 px-12 border-2 border-orange-500 text-orange-500 rounded-full font-semibold uppercase hover:bg-orange-500 hover:text-white shadow-md hover:shadow-lg transition"
+        className="flex items-center space-x-2 px-8 py-1 border-2 border-orange-500 text-orange-500 rounded-full font-semibold uppercase hover:bg-orange-500 hover:text-white shadow-md hover:shadow-lg transition "
       >
         <FaUtensils className="w-5 h-5" />
         <span>Menu</span>
@@ -17,7 +17,7 @@ export default function ActionButtons({ onAsk }) {
       
       <button
         onClick={onAsk}
-        className="flex items-center space-x-2 px-12 bg-orange-100 text-black rounded-full font-semibold uppercase hover:bg-orange-200 shadow-md hover:shadow-lg transition"
+        className="flex items-center space-x-2 px-8 py-1 bg-orange-100 text-black rounded-full font-semibold uppercase hover:bg-orange-200 shadow-md hover:shadow-lg transition"
       >
         <FaRobot className="w-5 h-5" />
         <span>Ask AI</span>
